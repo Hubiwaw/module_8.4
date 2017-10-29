73f682a0d6a46279ef679268e93de98008954c3e
Hubert Maćkowiak <mackowiak.h@wp.pl>
2017-10-29 12:29:05 +0100

    first commit on branch master

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var name = prompt('Enter your name');
+alert('Hello, ' + name);//first commnet reagarding to line with alert
+console.log('Hello, ' + name);//we can chceck if our script is correctly in console
+/*block comment only for test
+*/
